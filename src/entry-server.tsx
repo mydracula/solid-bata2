bfb084a88f0eb3d41229fe68fc33a5b3dae26fce
2458578637 <2458578637@qq.com>
2024-02-15 16:08:02 +0800

    ok

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -11,13 +11,13 @@ export default createHandler(() => (
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <title>热烈的杯</title>
           <link rel="icon" href="/favicon.ico" />
-          <link rel="stylesheet" href="https://jsdelivr.b-cdn.net/npm/@pqina/pintura/pintura.css" />
+          {/* <link rel="stylesheet" href="https://jsdelivr.b-cdn.net/npm/@pqina/pintura/pintura.css" /> */}
           {assets}
         </head>
         <body>
           <div id="app">{children}</div>
           <script src="https://jsdelivr.b-cdn.net/gh/mydracula/image@master/20240205/93273d94959848b49de98f593169704e.js"></script>
-          <script src="https://jsdelivr.b-cdn.net/npm/@pqina/pintura/pintura-umd.js"></script>
+          {/* <script src="https://jsdelivr.b-cdn.net/npm/@pqina/pintura/pintura-umd.js"></script> */}
           {scripts}
         </body>
       </html>
