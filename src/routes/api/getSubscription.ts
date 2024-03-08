f154ccd3b37879dc5aa86386dc408d2ff9535dfe
2458578637 <2458578637@qq.com>
2024-03-08 18:57:01 +0800

    ok

--- src/routes/api/getSubscription.ts
+++ src/routes/api/getSubscription.ts
@@ -8,18 +8,35 @@ const toBase64 = s => Buffer.from(s).toString('base64')
 export async function GET (event: APIEvent) {
   try {
     // const regex = /(\w+)=(https?:\/\/[^\s&]+)/g
-    const url = decodeURIComponent(event.request.url)
-    const regex = /(?:subscribe|config)=.*?(?=(?:&subscribe=|&config=|$))/g
-    let matches
-    const keyValuePairs = []
+    // const url = decodeURIComponent(event.request.url)
+    // const regex = /(?:subscribe|config)=.*?(?=(?:&subscribe=|&config=|$))/g
+    // let matches
+    // const keyValuePairs = []
     const result = []
-    while ((matches = regex.exec(url)) !== null) {
-      const index = matches[0].indexOf('=')
-      keyValuePairs.push({
-        key: matches[0].split('=')[0],
-        value: matches[0].substring(index + 1)
-      })
-    }
+    // while ((matches = regex.exec(url)) !== null) {
+    //   const index = matches[0].indexOf('=')
+    //   keyValuePairs.push({
+    //     key: matches[0].split('=')[0],
+    //     value: matches[0].substring(index + 1)
+    //   })
+    // }
+    const keyValuePairs = [
+      {
+        key: 'subscribe',
+        value:
+          'https://raw.githubusercontent.com/wentao883/TG-wxgqlfx_ZDY/main/pdyjdy_ZDY'
+      },
+      {
+        key: 'subscribe',
+        value:
+          'https://raw.githubusercontent.com/wentao883/TG-wxgqlfx_PDSX/main/pdyjdy_PDSX'
+      },
+      {
+        key: 'subscribe',
+        value:
+          'https://raw.githubusercontent.com/wentao883/TG-wxgqlfx_QZFL/main/pdyjdy_QZFL'
+      }
+    ]
 
     console.log(keyValuePairs, '😘')
     for (const item of keyValuePairs) {
