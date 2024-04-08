a4dc6afc4af382284f8f24e2feeb610a2cbc8105
2458578637 <2458578637@qq.com>
2024-04-09 00:45:03 +0800

    ok

--- src/routes/api/yunhu.ts
+++ src/routes/api/yunhu.ts
@@ -37,6 +37,7 @@ export async function POST (event: APIEvent) {
   getImageFileFromUrl(imageUrl, async function (imgFile) {
     if (!imgFile) return
     const formData = new FormData()
+    const timestamp = Date.now().toString()
     formData.append(
       'file',
       imgFile,
@@ -44,15 +45,38 @@ export async function POST (event: APIEvent) {
         ? imageName.replace(/\.(tmp|webp)$/, '.png')
         : imageName
     )
+    formData.append('timestamp', timestamp)
+    formData.append('nonce', 'WCLtScJD')
+    const locale = getSignature({
+      nonce: 'WCLtScJD',
+      timestamp
+    })
 
     console.log('开始请求')
 
     const response = await (
-      await fetch('https://yaohuo-peachStoat.web.val.run', {
+      await fetch('https://api.weixinyanxuan.com/mall/api/img/upload', {
         method: 'POST',
-        body: formData
+        body: formData,
+        headers: {
+          'Accept-Locale': locale,
+          Accept: 'application/json, text/plain, */*',
+          'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,zh-TW;q=0.7',
+          'Cache-Control': 'no-cache',
+          Connection: 'keep-alive',
+          DNT: '1',
+          Origin: 'https://b.haiduiyanxuan.cn',
+          Pragma: 'no-cache',
+          Referer: 'https://b.haiduiyanxuan.cn/',
+          'Sec-Fetch-Dest': 'empty',
+          'Sec-Fetch-Mode': 'cors',
+          'Sec-Fetch-Site': 'cross-site',
+          'User-Agent':
+            'Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1'
+        }
       })
     ).json()
+    
     console.log(response)
     // if (response.code !== '0') return
     // const { fileName, filePath } = response.data
@@ -127,4 +151,21 @@ async function sendYunHu (body: string) {
       }
     )
   ).json()
+}
+
+function getSignature (e) {
+  var t = 'fuck-your-mother-three-thousand-times-apes-not-kill-apes'
+  var n = Object.keys(e)
+    .sort()
+    .map(function (key) {
+      if (typeof e[key] !== 'object' && e[key] !== null) {
+        return key + '=' + e[key]
+      }
+    })
+    .filter(function (str) {
+      return str
+    })
+  var n = n.join('&')
+  var i = CryptoJS.MD5(n + t).toString()
+  return i
 }
