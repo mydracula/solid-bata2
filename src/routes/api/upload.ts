2e0bfd3494d3215701fb34a9af4c41421aff1f56
2458578637 <2458578637@qq.com>
2024-08-30 00:59:23 +0800

    ok

--- src/routes/api/upload.ts
+++ src/routes/api/upload.ts
@@ -1,37 +1,40 @@
 import axios from 'axios'
 import type { APIEvent } from "@solidjs/start/server";
 export async function POST(event: APIEvent) {
-    try {
-        const body = await event.request.formData();
-        const ck = body.get('ck') as string
-        const response = await axios.post('https://f.hatena.ne.jp/upbysmart', body, {
-            headers: {
-                'Content-Type': 'multipart/form-data',
-                'Cookie': ck,
-            },
-        });
-        const { data, status } = response;
-        if (data?.image_url) {
-            return new Response(JSON.stringify(data), {
-                status,
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            });
-        } else {
-            return new Response('Failed to upload image', {
-                status: 500,
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            });
-        }
-    } catch (error) {
-        return new Response('Failed to upload image', {
-            status: 500,
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
+  try {
+    const body = await event.request.formData();
+    const ck = body.get('ck') as string
+    const response = await axios.post('https://f.hatena.ne.jp/upbysmart', body, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+        'Cookie': ck,
+      },
+    });
+    console.log(response, 'response');
+
+    const { data, status } = response;
+    if (data?.image_url) {
+      return new Response(JSON.stringify(data), {
+        status,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    } else {
+      return new Response('Failed to upload image', {
+        status: 500,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
     }
+  } catch (error) {
+    console.log(error, 'response');
+    return new Response('Failed to upload image', {
+      status: 500,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
 }
